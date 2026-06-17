e20571c38b8cb25b5d08f05cebc6cf5dca779359
espejosara <espejo.sara93@gmail.com>
2026-06-17 13:47:57 +0200

    Añadir endpoint GET / para obtener todos los usuarios (READ)

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User.js"); // Importamos nuestro molde
+const User = require("../models/User.js"); 
 
+// Endpoint para crear un usuario (CREATE)
 router.post("/create", async (req, res) => {
     try {
         const user = await User.create(req.body);
@@ -10,6 +11,18 @@ router.post("/create", async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).send({ message: "Hubo un problema al intentar crear un usuario" });
+    }
+});
+
+// Endpoint para leer todos los usuarios (READ)
+router.get("/", async (req, res) => {
+    try {
+        // .find() sin nada dentro significa "búscame TODOS los documentos de esta colección"
+        const users = await User.find(); 
+        res.status(200).send(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Hubo un problema al obtener los usuarios" });
     }
 });
 
